b24e7bed0c4fca0218f22fe40730a05e4b696569
Lukas <lukas.heinrich@gmail.com>
2015-02-10 19:21:29 -0500

    Update general_rivet_blueprint.py

--- recastrivet/general_rivet_blueprint.py
+++ recastrivet/general_rivet_blueprint.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, jsonify, request, send_from_direct
 blueprint = Blueprint('general_rivet', __name__, template_folder='general_rivet_templates')
 
 RECAST_ANALYSIS_ID = 'general_rivet'
+RECASTSTORAGEPATH = '/home/analysis/recast/recaststorage'
 
 import json
 import requests
@@ -10,14 +11,14 @@ import os
 from zipfile import ZipFile
 import glob
 
-@blueprint.route('/result/<requestId>/<parameter_pt>')
+@blueprint.route('/t/<requestId>/<parameter_pt>')
 def result_view(requestId,parameter_pt):
   analysis = '*'
 
-  analyses = filter(os.path.isdir,glob.glob('rivet_results/{}/{}/plots/*'.format(requestId,parameter_pt)))
+  analyses = filter(os.path.isdir,glob.glob('{}/results/{}/{}/plots/*'.format(RECASTSTORAGEPATH,requestId,parameter_pt)))
 
   print analyses
-  print ['rivet_results/{}/{}/plots/{}/*.dat'.format(requestId,parameter_pt,a) for a in analyses]
+  print ['{}/results/{}/{}/plots/{}/*.dat'.format(RECASTSTORAGEPATH,requestId,parameter_pt,a) for a in analyses]
 
   plotdict = {os.path.basename(a):[os.path.basename(p).rsplit('.',1)[0] for p in glob.glob('{}/*.dat'.format(a))] for a in analyses}
 
@@ -27,7 +28,7 @@ def result_view(requestId,parameter_pt):
 
 @blueprint.route('/plot/<requestId>/<parameter_pt>/<path:file>')
 def plot_server(requestId,parameter_pt,file):
-  filepath = 'rivet_results/{}/{}/plots/{}'.format(requestId,parameter_pt,file)
+  filepath = '{}/results/{}/{}/plots/{}'.format(RECASTSTORAGEPATH,requestId,parameter_pt,file)
   print filepath
   return send_from_directory(os.path.dirname(filepath),os.path.basename(filepath))
   
