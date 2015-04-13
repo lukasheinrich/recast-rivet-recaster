bc68596f39e362f43553d9ef5396a5bee7653539
Lukas Heinrich <lukas.heinrich@cern.ch>
2015-04-13 03:52:23 +0200

    bootstrap scripts

---
+++ mk_analysis.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+set -e
+
+echo "such a generic plugin does not need much setup. Bye."
---
+++ setup_analysis.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+#fail on error
+set -e
+
+#we need to work from this subdir for now. so cd into it
+cd implementation
+
+#done
